875fb20c9eb16a50a1b898730f6e383378b27cc7
SVPopov93 <SVPopov93@gmail.com>
2019-03-08 00:13:37 +0400

    upd: created based layout (did not implement functionality)

--- CRUD/ClientApp/src/components/Home.js
+++ CRUD/ClientApp/src/components/Home.js
@@ -1,10 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { compose } from 'redux';
 
-const Home = props => (
-  <div>
-    <h1>Hello, world!</h1>
-  </div>
-);
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import MiniDrawer from './MiniDrawer/index';
 
-export default connect()(Home);
+const styles = theme => ({
+    root: {
+        flexGrow: 1,
+    },
+    paper: {
+        padding: theme.spacing.unit * 2,
+        textAlign: 'center',
+        color: theme.palette.text.secondary,
+    },
+});
+
+class Home extends Component {
+
+    render() {
+        return (
+            <Grid container spacing={24}>
+                <Grid item xs={12}>
+                    <h3>SPA</h3>
+                </Grid>
+                <Grid item xs={12}>
+                    <MiniDrawer />
+                </Grid>
+            </Grid>
+        )
+    }
+}
+
+Home.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = state => ({
+
+});
+
+export default compose(
+    withStyles(styles),
+    connect(mapStateToProps),
+)(Home);
---
+++ CRUD/ClientApp/src/components/MiniDrawer/index.jsx
@@ -0,0 +1,219 @@
+﻿import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import { withStyles } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import MailIcon from '@material-ui/icons/Mail';
+import { Button } from '@material-ui/core';
+
+import Fab from '@material-ui/core/Fab';
+import AddIcon from '@material-ui/icons/Add';
+import Icon from '@material-ui/core/Icon';
+import EditIcon from '@material-ui/icons/Edit';
+import DeleteIcon from '@material-ui/icons/Delete';
+import NavigationIcon from '@material-ui/icons/Navigation';
+
+import TableCustom from '../TableCustom/index';
+
+const drawerWidth = 240;
+
+const styles = theme => ({
+    root: {
+        display: 'flex',
+    },
+    fab: {
+        margin: theme.spacing.unit,
+    },
+    appBar: {
+        zIndex: theme.zIndex.drawer + 1,
+        transition: theme.transitions.create(['width', 'margin'], {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen,
+        }),
+    },
+    appBarShift: {
+        marginLeft: drawerWidth,
+        width: `calc(100% - ${drawerWidth}px)`,
+        transition: theme.transitions.create(['width', 'margin'], {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.enteringScreen,
+        }),
+    },
+    menuButton: {
+        marginLeft: 12,
+        marginRight: 36,
+    },
+    hide: {
+        display: 'none',
+    },
+    drawer: {
+        width: drawerWidth,
+        flexShrink: 0,
+        whiteSpace: 'nowrap',
+    },
+    drawerOpen: {
+        width: drawerWidth,
+        transition: theme.transitions.create('width', {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.enteringScreen,
+        }),
+    },
+    drawerClose: {
+        transition: theme.transitions.create('width', {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen,
+        }),
+        overflowX: 'hidden',
+        width: theme.spacing.unit * 7 + 1,
+        [theme.breakpoints.up('sm')]: {
+            width: theme.spacing.unit * 9 + 1,
+        },
+    },
+    toolbar: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'flex-end',
+        padding: '0 8px',
+        ...theme.mixins.toolbar,
+    },
+    content: {
+        flexGrow: 1,
+        padding: theme.spacing.unit * 3,
+    },
+});
+
+class MiniDrawer extends React.Component {
+    state = {
+        open: false,
+    };
+
+    handleDrawerOpen = () => {
+        this.setState({ open: true });
+    };
+
+    handleDrawerClose = () => {
+        this.setState({ open: false });
+    };
+
+    render() {
+        const { classes, theme } = this.props;
+
+        return (
+            <div className={classes.root}>
+                <CssBaseline />
+                <AppBar
+                    position="fixed"
+                    className={classNames(classes.appBar, {
+                        [classes.appBarShift]: this.state.open,
+                    })}
+                >
+                    <Toolbar disableGutters={!this.state.open}>
+                        <IconButton
+                            color="inherit"
+                            aria-label="Open drawer"
+                            onClick={this.handleDrawerOpen}
+                            className={classNames(classes.menuButton, {
+                                [classes.hide]: this.state.open,
+                            })}
+                        >
+                            <MenuIcon />
+                        </IconButton>
+                        <Typography variant="h6" color="inherit" noWrap>
+                            Mini variant drawer
+                        </Typography>
+                    </Toolbar>
+                </AppBar>
+                <Drawer
+                    variant="permanent"
+                    className={classNames(classes.drawer, {
+                        [classes.drawerOpen]: this.state.open,
+                        [classes.drawerClose]: !this.state.open,
+                    })}
+                    classes={{
+                        paper: classNames({
+                            [classes.drawerOpen]: this.state.open,
+                            [classes.drawerClose]: !this.state.open,
+                        }),
+                    }}
+                    open={this.state.open}
+                >
+                    <div className={classes.toolbar}>
+                        <IconButton onClick={this.handleDrawerClose}>
+                            {
+                                //theme.direction === 'rtl' ? <MenuIcon /> : <MailIcon />
+                                theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />
+                            }
+                        </IconButton>
+                    </div>
+                    <Divider />
+                    {[
+                        <AddIcon text={'Add'} />,
+                        <EditIcon text={'Edit'}/>,
+                        <DeleteIcon text={'Delete'} />
+                    ].map((component, index) =>
+                        <section key={index}>
+                            <Fab size="small" color="primary" aria-label="text" className={classes.fab}>
+                                {component}
+                            </Fab>
+                            <span style={{
+                                marginLeft: '24px',
+                                fontSize: '28px',
+                                fontWeight: 'bold',
+                                color: '#3f51b5' }} >
+                                {component.props.text}
+                            </span>
+                        </section>
+                    )}
+                    <Divider />
+                </Drawer>
+                <main className={classes.content}>
+                    <div className={classes.toolbar} />
+                    <Typography paragraph>
+                            {/*
+                            */}
+                        <TableCustom
+                            Items={[
+                                { number: 1, name: 'qwe', department: 'Dev' },
+                                { number: 2, name: 'qaz', department: 'QA' },
+                                { number: 3, name: 'asd', department: 'UI' },
+                                { number: 4, name: 'cxz', department: 'IT' }
+                            ]}
+                            Headers={
+                                ['#', 'User Name', 'Department']
+                            }
+                        />
+                    </Typography>
+                    <Typography paragraph>
+                        Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper eget nulla
+                        facilisi etiam dignissim diam. Pulvinar elementum integer enim neque volutpat ac
+                        tincidunt. Ornare suspendisse sed nisi lacus sed viverra tellus. Purus sit amet volutpat
+                        consequat mauris. Elementum eu facilisis sed odio morbi. Euismod lacinia at quis risus
+                        sed vulputate odio. Morbi tincidunt ornare massa eget egestas purus viverra accumsan in.
+                        In hendrerit gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem
+                        et tortor. Habitant morbi tristique senectus et. Adipiscing elit duis tristique
+                        sollicitudin nibh sit. Ornare aenean euismod elementum nisi quis eleifend. Commodo
+                        viverra maecenas accumsan lacus vel facilisis. Nulla posuere sollicitudin aliquam
+                        ultrices sagittis orci a.
+                    </Typography>
+                </main>
+            </div>
+        );
+    }
+}
+
+MiniDrawer.propTypes = {
+    classes: PropTypes.object.isRequired,
+    theme: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles, { withTheme: true })(MiniDrawer);
