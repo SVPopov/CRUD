6c2963c9436ec31ba0d1e94b51bb3956dd5cc6b7
SVPopov93 <SVPopov93@gmail.com>
2019-03-10 19:54:33 +0400

    upd: added user & department to EF migrations
    fix: typo fix Titile -> Title

---
+++ CRUD.Repositories/Migrations/20190310153238_TypoFixTitle.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CRUD.Repositories.Migrations
+{
+    public partial class TypoFixTitle : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Titile",
+                table: "Department",
+                newName: "Title");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Title",
+                table: "Department",
+                newName: "Titile");
+        }
+    }
+}
